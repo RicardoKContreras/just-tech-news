6e4c88c45cc8a76fd4bf36618b46ec0cb8191bb4
Jimbei <contrerasr2272@gmail.com>
2022-09-12 22:46:32 -0400

    setting up authentications

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require('express');
+const session = require('express-session');
 const routes = require('./controllers/');
 const sequelize = require('./config/conection.js');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 const app = express();
+//session is used for cookies to save data and connects to the Sequelize database.
+
 const PORT = process.env.PORT || 3001;
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
